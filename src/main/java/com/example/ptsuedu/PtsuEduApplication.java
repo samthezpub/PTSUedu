d4dc7b872698276dc9367bd562d10b6559637922
Zpub <lil.rhymes9@gmail.com>
2024-01-18 23:11:15 +0300

    git init

---
+++ src/main/java/com/example/ptsuedu/PtsuEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.ptsuedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtsuEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtsuEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ptsuedu/PtsuEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ptsuedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtsuEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
